4424662de80cf87e9e38d35fce688998fa7deb07
dazedDive <dazed_dive@hotmail.fr>
2022-09-02 09:24:57 +0200

    fin du debut du tp

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -6,7 +6,7 @@ const CategoryCard =({title,description,image})=>{
     return(
         <>
         <div className="col-6 col-md-3">
-        <div class="card">
+        <div className="card shadow p-3 mb-5 bg-body rounded">
         <img src={image} class="card-img-top" alt="..."/>
         <div className="card-body">
         <h5 className="card-title">{title}</h5>
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -2,15 +2,28 @@ import CategoryCard from "../components/CategoryCard"
 
 const HomeScreen =()=>{
 
+    const categoryPool =
+     [{id:1,image:"https://picsum.photos/id/237/200/300",title:"Heavy Metal",description:"du text"},
+     {id:2,image:"https://picsum.photos/id/232/200/300",title:"Death Metal",description:"autre text"},
+     {id:3,image:"https://picsum.photos/id/231/200/300",title:"Grind Core",description:"encore du text"},
+     {id:4,image:"https://picsum.photos/id/236/200/300",title:"Industrial",description:"lorem 15"},
+
+    ]
+    const categoriesList = categoryPool.map(categorie=><CategoryCard 
+    key={categorie.id} 
+    image={categorie.image}
+    title={categorie.title}
+    description={categorie.description}/>)
 
 
     return(
         <>
-        <h3 className="text-danger">Extrem Metal Vinyle Store</h3>
+        <div className="container-fluid bg-danger">
+        <h3 className="text-dark py-3">Extrem Metal Vinyle Store</h3>
+        </div>
         <div className="container-fluid">
             <div className="row">
-               <CategoryCard/>
-               <CategoryCard/>
+               {categoriesList}
             </div>
         </div>
         </>
